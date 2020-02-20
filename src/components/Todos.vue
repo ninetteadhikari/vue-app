4dcfddae4b7593ea696680ce0452f18b4132d3ee
Ninette <ninette.adhikari@gmail.com>
2020-02-20 15:45:43 +0100

    initial setup

---
+++ src/components/Todos.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div v-for="todo in todos">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todos",
+  props:["todos"]
+};
+</script>
+
+<style scoped>
+</style>
